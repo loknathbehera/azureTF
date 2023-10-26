e245b39b825e798a14a1f922ee5a0337183402c0
Loknath <lokanath60@gmail.com>
2023-10-26 21:03:09 +0530

    sf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "example" {
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network2"
+  name                = "example-network23"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
